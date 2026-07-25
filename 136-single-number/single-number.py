f74a8b5b8fb7084a19b4a0b205bf8ffcd59af3b4
Pavan P <pavannayaka4@gmail.com>
2026-07-25 18:52:50 +0530

    Time: 13 ms (12.98%) | Memory: 21.6 MB (18.46%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        h={}
+
+        for i in nums:
+            if i in h.keys():
+                h[i]=h[i]+1
+            else:
+                h[i]=1
+        
+        for i in h.keys():
+            if h[i]==1:
+                return i
+        
