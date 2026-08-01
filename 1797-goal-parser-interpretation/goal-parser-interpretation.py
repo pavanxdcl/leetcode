9ea3ea4a1306257699a74ce23ed5595f5437909b
Pavan P <pavannayaka4@gmail.com>
2026-08-01 13:04:00 +0530

    Time: 34 ms (96.39%) | Memory: 19.3 MB (43.23%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def interpret(self, c: str) -> str:
+        s=[]
+        ans=""
+        for i in c:
+            if i=='G':
+                ans=ans+i
+            elif i=='(':
+                s.append(i)
+            elif i==')':
+                if s[-1]=='(':
+                    ans=ans+'o'
+                else:
+                    ans=ans+"al"
+                    while(s[-1]!='('):
+                        s.pop()
+                    s.pop()
+            else:
+                s.append(i)
+        
+        return ans
