d793c69d62f392852e93154d5e3505b8017ced6a
Pavan P <pavannayaka4@gmail.com>
2026-08-11 22:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (17.01%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> v;
+
+        for(auto i:nums){
+            v.push_back(i);
+        }
+
+        for(auto i:nums){
+            v.push_back(i);
+        }
+
+        return v;
+
+    }
+};
